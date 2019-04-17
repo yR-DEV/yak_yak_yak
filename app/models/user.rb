15bac90619d2a81d2f54b1a0c9e3969875c1709d
yeah-right <claar.zack@gmail.com>
2019-04-17 16:01:26 -0600

    Set up users model and controller and a view. Rails server running locally with no problems. Going to to commit and push to have a fallback commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one :job_tracker
+  has_many :sent_apps, through: :job_tracker
+
+  has_one :resume 
+
+  has_many :skills, through: :resume
+  has_one :bio, through: :resume
+  has_many :educations, through: :resume
+  has_many :job_histories, through: :resume
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: "users#index"
+  
+  resources :users do 
+    resources :resumes
+    resources :job_trackers
+  end 
 end
